9e0b33d151a3721a582691ae96f0274f6e624f35
caleb-kylib <lemayiancaleb18@gmail.com>
2025-07-13 23:13:58 +0300

    Fix output formatting to two decimal places

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,5 @@ expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = income - expenses
 projected_savings = monthly_savings * 12 * 1.05
 
-print(f"Your monthly savings are ${int(monthly_savings)}.")
-print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
